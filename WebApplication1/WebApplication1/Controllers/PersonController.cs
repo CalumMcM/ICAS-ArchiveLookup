fb4ba5d9197f3f9a4b27ac0eff5d3f29ad206262
cmcmeekin <cmcmeekin@icas.com>
2019-07-31 11:59:53 +0100

    Able to query database

--- WebApplication1/WebApplication1/Controllers/PersonController.cs
+++ WebApplication1/WebApplication1/Controllers/PersonController.cs
@@ -5,16 +5,39 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApplication1.Models;
+using Dapper;
+using System.Web.Configuration;
+using System.Data.SqlClient;
 
 namespace WebApplication1.Controllers
 {
     public class PersonController : ApiController
     {
-		public List <Person> Post(PersonQuery query)
+		//public List <Person> Post(PersonQuery query)
+		public List<Person> Get(string query)
 		{
-			var newPerson = new List<Person>();
-			//Use Dapper to call the database
-			return newPerson;
+			var persons = new List<Person>();
+			
+			using (var connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["ArchiveLookup"].ConnectionString))
+			{
+				var sql = @"SELECT	n.[ID]
+								,n.[MAJOR_KEY]
+								,n.[FULL_NAME]
+								,n.[FIRST_NAME]
+								,n.[LAST_FIRST]
+								,n.[COMPANY]
+								,n.[MEMBER_TYPE]
+								,mt.[DESCRIPTION]
+								,n.[EMAIL]
+								FROM [imis].[dbo].[Name] as n
+								JOIN activity as a
+								on n.id = a.id
+								JOIN Member_Types as mt
+								on n.MEMBER_TYPE  = mt.MEMBER_TYPE
+								WHERE a.id = @query"; //Will this use the string query variable?
+				persons = connection.Query<Person>(sql, new { query = query }).ToList();
+			}
+			return persons;
 		} 
     }
 
--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -16,9 +16,9 @@ namespace WebApplication1
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
 			GlobalConfiguration.Configure(WebApiConfig.Register);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
     }
 }
