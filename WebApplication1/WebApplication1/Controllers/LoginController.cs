e5b5117c84ce37f4576be5d61f4054eb9181f3f7
cmcmeekin <cmcmeekin@icas.com>
2019-08-06 09:33:18 +0100

    Made login page

---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ArchiveLookup.ICAS.com.Controllers
+{
+    public class LoginController : ApiController
+    {
+		public bool Post([FromBody]string pin)
+		{
+			return false;
+		}
+    }
+}
