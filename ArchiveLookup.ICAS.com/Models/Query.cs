2034cc83694f0b5530e5f7bba6bb5ae47b20028e
cmcmeekin <cmcmeekin@icas.com>
2019-08-22 15:44:36 +0100

    Fixed bug where unable to search by date

--- ArchiveLookup.ICAS.com/Models/Query.cs
+++ ArchiveLookup.ICAS.com/Models/Query.cs
@@ -19,14 +19,33 @@ namespace ArchiveLookup.ICAS.com.Models
 			bool began = false;
 			for (var i = 0; i < properties.Length; i++)
 			{
-				if ((string) (properties[i].GetValue(this)) != "" && properties[i].GetValue(this) != null && !began)
+				if (properties[i].GetValue(this) != null)
 				{
-					query = query + "WHERE " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
-					began = true;
-				}
-				else if (((string) properties[i].GetValue(this)) != "" && properties[i].GetValue(this) != null)
-				{
-					query = query + " AND " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+					var type = properties[i].GetValue(this);
+					if(type is string)
+					{
+						if ((string)(properties[i].GetValue(this)) != "" && !began)
+						{
+							query = query + "WHERE " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+							began = true;
+						}
+						else if (((string)properties[i].GetValue(this)) != "")
+						{
+							query = query + " AND " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+						}
+					}
+					if(type is DateTime)
+					{
+						if (!began)
+						{
+							query = query + "WHERE " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+							began = true;
+						}
+						else
+						{
+							query = query + " AND " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+						}
+					}
 				}
 			}
 			return query;
