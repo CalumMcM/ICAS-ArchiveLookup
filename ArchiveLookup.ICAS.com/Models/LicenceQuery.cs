92275975312233d00e5c1b2a7c8a3c56ed7ac215
cmcmeekin <cmcmeekin@icas.com>
2019-08-23 10:30:00 +0100

    Added feature to now get firm names similar to name submitted

--- ArchiveLookup.ICAS.com/Models/LicenceQuery.cs
+++ ArchiveLookup.ICAS.com/Models/LicenceQuery.cs
@@ -68,6 +68,44 @@ namespace ArchiveLookup.ICAS.com.Models
 		/*
 		 Returns: A string, in the SQL format, containing any criteria to filter the search 
 		*/
+		public virtual string queryGenerator()
+		{
+			var properties = this.GetType().GetFields();
+			var query = "";
+			bool began = false;
+			for (var i = 0; i < properties.Length; i++)
+			{
+				if (properties[i].GetValue(this) != null)
+				{
+					var type = properties[i].GetValue(this);
+					if (type is string)
+					{
+						if (properties[i].Name == "SORT_NAME")
+						{
+							if (!began)
+							{
+								query = query + "WHERE " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " like '%'+@" + properties[i].Name +"+'%'";
+								began = true;
+							}
+							else
+							{
+								query = query + " AND " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " like '%'+@" + properties[i].Name + "+'%'";
+							}
+						}
+						else if ((string)(properties[i].GetValue(this)) != "" && !began)
+						{
+							query = query + "WHERE " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+							began = true;
+						}
+						else if (((string)properties[i].GetValue(this)) != "")
+						{
+							query = query + " AND " + this.getDatabasePrefix(properties[i].Name) + properties[i].Name + " = @" + properties[i].Name;
+						}
+					}
+				}
+			}
+			return query;
+		}
 	}
 
 }
